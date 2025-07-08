2427fed2d6d0c6c5b1cffdf41b70ea0ee04ea4f1
tjcasllt <thanakrit.ph@ocean.co.th>
2025-07-08 16:45:47 +0700

    upload code
    - เพิ่ม data alteration
    - test case เพิ่ม function map data
    - test case เพิ่ม function upload data ขึ้น google sheet
    - แก้ไข locator alteration

--- data/Alteration/inquiryform.data.js
+++ data/Alteration/inquiryform.data.js
@@ -1,6 +1,9 @@
 export const inquiryformArraykey_label = [
     {
         policyno: '0233590',
-
+        SELECTOR_Alteration_MENU_SUB_1_In_Page_2_Detail_Panel_Data: [{
+            label: 'ชื่อ-นามสกุล',
+            data: [['ซีเอสอง ยูพันแอล']]
+        }]
     }
 ]
