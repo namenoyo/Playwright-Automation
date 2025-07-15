e2c1091837080b7bae5c29b958b8503cc0bb3d3f
tjcasllt <thanakrit.ph@ocean.co.th>
2025-07-15 16:43:38 +0700

    update code
    1. เพิ่ม key ของไฟล์ data
        - query
        - wherefield
    2. ไฟล์ test case ทดสอบการเชื่อมต่อ database
    3. เพิ่ม function การเช็คข้อมูลจาก database
    4. เพิ่ม function maps data ของข้อมูล database

--- e2e/connectDB.spec.js
+++ e2e/connectDB.spec.js
@@ -1,7 +1,19 @@
 import { test, expect } from '@playwright/test'
 const db = require('../database/database')
 
-test('', async ({ page }) => {
+test('test show data on query database', async ({ page }) => {
+    // ใช้ query และ parameter where ตรงๆ
     const result = await db.query('SELECT * FROM bk_grid_label_expected g WHERE g.label = $1 AND g.id = $2', ['Payment History', '1'])
     console.log(result.rows[0].label)
+
+    // ใช้ query และ parameter where ผ่านตัวแปร
+    const query = 'SELECT * FROM bk_grid_label_expected g'
+    const wherefield = ['Payment History']
+    const resultvariable = await db.query(query)
+    console.log(resultvariable.rows[0].label)
+
+    // แปลง object เป็น array
+    const resultchangeobj = resultvariable.rows.map(obj => Object.values(obj));
+    console.log(resultchangeobj);
+    console.log(resultchangeobj.length);
 })
