cffecbb6fcc9774ec203ac0eb4e880c02fc185f1
namenoyo <namenoyo@gmail.com>
2025-07-16 14:42:04 +0700

    Push code
    - ทำ file Array Test ชื่อ inquiryform_from_Data_Mapping.data.js

--- data/Alteration/inquiryform.data.js
+++ data/Alteration/inquiryform.data.js
@@ -6,6 +6,13 @@ export const inquiryformArraykey_label = [
             data: [['ซีเอสอง ยูพันแอล']],
             query: 'SELECT * FROM bk_grid_label_expected g WHERE g.label = $1',
             wherefield: ['Payment History']
+        }],
+        policyno: '0233590',
+        SELECTOR_Alteration_MENU_SUB_1_In_Page_2_Detail_Panel_Data: [{
+            label: 'ชื่อ-นามสกุล',
+            data: [['ซีเอสอง ยูพันแอล']],
+            query: 'SELECT * FROM bk_grid_label_expected g WHERE g.label = $1',
+            wherefield: ['Payment History']
         }]
     }
 ]
---
+++ data/Alteration/inquiryform_from_Data_Mapping.data.js
@@ -0,0 +1,160 @@
+export const inquiryformArraykey_label = [
+  {
+    policy_no: '1652002',
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_2_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel ข้อมูลผู้เอาประกันภัย  > ชื่อ - นามสกุล', data: [['นายเจทีพันบี ดีเก้าพีบี MAL']]}
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_4_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel ข้อมูลผู้เอาประกันภัย  > ประเภทบัตร', data: [['บัตรประชาชน/ตัวประชาชน 13 หลัก']] },
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel ข้อมูลผู้เอาประกันภัย  > เลขที่บัตร', data: [['3-801X-XXXX6-912']]  }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_6_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel ข้อมูลผู้เอาประกันภัย  > สัญชาติ', data: [['ไทย']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_8_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel ข้อมูลผู้เอาประกันภัย  > วันเดือนปี เกิด',data: [['28/07/2504']]  }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_10_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel ข้อมูลผู้เอาประกันภัย  > อายุ ณ วันเริ่มสัญญา',data: [['62 ปี']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_12_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel ข้อมูลผู้เอาประกันภัย  > อายุ ปัจจุบัน',data: [['64 ปี']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_14_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel ข้อมูลผู้เอาประกันภัย  > สาขาต้นสังกัด',data: [['7000 : อุทัยธานี']]}
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_16_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel ข้อมูลผู้เอาประกันภัย  > เลขที่กรมธรรม์',data: [['1652002']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_18_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel ข้อมูลผู้เอาประกันภัย  > ประเภทกรมธรรม์', data: [['สามัญ']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_20_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel ข้อมูลผู้เอาประกันภัย  > สถานะกรมธรรม์', data: [['Lapse']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_22_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel ข้อมูลผู้เอาประกันภัย  > แบบประกัน', data: [['539 เพื่อนคู่ชีวิต B450 (20/15)']]}
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_24_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel ข้อมูลผู้เอาประกันภัย  > ทุนประกันหลัก', data: [['120,000.00 บาท']]}
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_26_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel ข้อมูลผู้เอาประกันภัย  > เบี้ยประกันหลัก', data: [['922.80 บาท']]}
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_28_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel ข้อมูลผู้เอาประกันภัย  > งวดชำระ', data: [['รายเดือน']]}
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_32_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel ข้อมูลผู้เอาประกันภัย  > วันเริ่มสัญญา', data: [['10/03/2566']]}
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_34_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel ข้อมูลผู้เอาประกันภัย  > ระยะเวลาเอาประกัน',data: [['20 ปี']]}
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_36_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel ข้อมูลผู้เอาประกันภัย  > ระยะเวลาชำระเบี้ย',data: [['15 ปี']]}
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_38_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel ข้อมูลผู้เอาประกันภัย  > วันครบสัญญา',data: [['10/03/2586']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_40_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel ข้อมูลผู้เอาประกันภัย  > วันครบชำระเบี้ย', data: [['10/03/2581']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_2_In_Page_2_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel  รายละเอียดการชำระล่าสุด > วันที่ชำระ', data: [['17/01/2568']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_2_In_Page_4_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel  รายละเอียดการชำระล่าสุด > เลขที่ใบเสร็จ', data: [['02/11']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_2_In_Page_6_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel  รายละเอียดการชำระล่าสุด > ปีที/งวดที่', data: [['10000004994267']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_2_In_Page_8_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel  รายละเอียดการชำระล่าสุด > วันที่ชำระตั้งแต่', data: [['10/01/2568']]}
+    ],
+    SELECTOR_Alteration_MENU_SUB_2_In_Page_10_Detail_Panel_Data: [
+      {label: 'หน้าจอ ค้นหาใบสอบถาม> ดูรายละเอียด> Panel  รายละเอียดการชำระล่าสุด > วันที่ชำระถึง', data: [['09/02/2568']]}
+    ],
+
+  // เขตแบ่งข้อมูล
+    policy_no: '0233590',
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_2_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel ข้อมูลผู้เอาประกันภัย > ชื่อ - นามสกุล',  data: [['นายเจทีพันบี ดีเก้าพีบี MAL']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_4_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel ข้อมูลผู้เอาประกันภัย > ประเภทบัตร', data: [['บัตรประชาชน/ตัวประชาชน 13 หลัก']] },
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel ข้อมูลผู้เอาประกันภัย > เลขที่บัตร', data: [['3-801X-XXXX6-912']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_6_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel ข้อมูลผู้เอาประกันภัย > สัญชาติ', data: [['ไทย']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_8_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel ข้อมูลผู้เอาประกันภัย > วันเดือนปี เกิด', data: [['28/07/2504']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_10_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel ข้อมูลผู้เอาประกันภัย > อายุ ณ วันเริ่มสัญญา', data: [['62 ปี']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_12_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel ข้อมูลผู้เอาประกันภัย > อายุ ปัจจุบัน', data: [['64 ปี']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_14_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel ข้อมูลผู้เอาประกันภัย > สาขาต้นสังกัด', data: [['7000 : อุทัยธานี']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_16_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel ข้อมูลผู้เอาประกันภัย > เลขที่กรมธรรม์', data: [['1652002']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_18_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel ข้อมูลผู้เอาประกันภัย > ประเภทกรมธรรม์', data: [['สามัญ']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_20_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel ข้อมูลผู้เอาประกันภัย > สถานะกรมธรรม์', data: [['Lapse']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_22_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel ข้อมูลผู้เอาประกันภัย > แบบประกัน', data: [['539 เพื่อนคู่ชีวิต B450 (20/15)']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_24_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel ข้อมูลผู้เอาประกันภัย > ทุนประกันหลัก', data: [['120,000.00 บาท']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_26_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel ข้อมูลผู้เอาประกันภัย > เบี้ยประกันหลัก', data: [['922.80 บาท']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_28_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel ข้อมูลผู้เอาประกันภัย > งวดชำระ', data: [['รายเดือน']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_32_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel ข้อมูลผู้เอาประกันภัย > วันเริ่มสัญญา', data: [['10/03/2566']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_34_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel ข้อมูลผู้เอาประกันภัย > ระยะเวลาเอาประกัน', data: [['20 ปี']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_36_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel ข้อมูลผู้เอาประกันภัย > ระยะเวลาชำระเบี้ย', data: [['15 ปี']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_38_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel ข้อมูลผู้เอาประกันภัย > วันครบสัญญา', data: [['10/03/2586']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_1_In_Page_40_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel ข้อมูลผู้เอาประกันภัย > วันครบชำระเบี้ย', data: [['10/03/2581']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_2_In_Page_2_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel รายละเอียดการชำระล่าสุด > วันที่ชำระ', data: [['17/01/2568']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_2_In_Page_4_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel รายละเอียดการชำระล่าสุด > เลขที่ใบเสร็จ', data: [['02/11']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_2_In_Page_6_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel รายละเอียดการชำระล่าสุด > ปีที/งวดที่', data: [['10000004994267']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_2_In_Page_8_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel รายละเอียดการชำระล่าสุด > วันที่ชำระตั้งแต่', data: [['10/01/2568']] }
+    ],
+    SELECTOR_Alteration_MENU_SUB_2_In_Page_10_Detail_Panel_Data: [
+      { label: 'หน้าจอ ค้นหาใบสอบถาม > ดูรายละเอียด > Panel รายละเอียดการชำระล่าสุด > วันที่ชำระถึง', data: [['09/02/2568']] }
+    ]
+  },
+
+  
+]
+
+
+
+
