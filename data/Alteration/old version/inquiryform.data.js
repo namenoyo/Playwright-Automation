22ba0052fdf7bf9ec141769adbf0a4ee42fc97fe
namenoyo <rangsiman.ph@ocean.co.th>
2025-10-29 11:04:06 +0700

    push up
    - update  data_endorse_doc_v1.data.js
    ให้มีข้อมูลสำหรับการทดสอบบันทึกสอบถามสลักหลัง
    ข้อมูลทดสอบ 10,725  แบ่ง Index 0-5500 และ 5501-10725
    
    set 1
    ECN01 ✅
    ECN02 ✅
    ECN03 ✅
    ECN08 ✅
    ECN12 ✅
    ECN14 ✅

---
+++ data/Alteration/old version/inquiryform.data.js
@@ -0,0 +1,12 @@
+export const inquiryformArraykey_label = [
+    {
+        policy_no: '0233590', // 0233590
+        SELECTOR_Alteration_MENU_SUB_1_In_Page_2_Detail_Panel_Data: [{
+            label: 'ชื่อ-นามสกุล',
+            data: [['ซีเอสอง ยูพันแอล']],
+            query: 'SELECT * FROM bk_grid_label_expected g WHERE g.label = $1',
+            wherefield: ['Payment History'],
+            fieldneeds: ['label'],
+        }],
+    },
+]
---
+++ data/Alteration/old version/raw_data_alteration.data.js
@@ -0,0 +1,54 @@
+export const raw_data_alteration = {
+    "BRN": {
+        "I": {
+            "ALL": {
+                "0": {
+                    "AGT": [
+                        {
+                            endorse_code: "ECN01",
+                            endorse_name: "เปลี่ยนแปลงคำนำหน้าชื่อ ชื่อ - นามสกุล ผู้เอาประกัน",
+                            endorse_checkbox: "enable",
+                        },
+                        {
+                            endorse_code: "ECN02",
+                            endorse_name: "เปลี่ยนแปลงที่อยู่ ผู้เอาประกัน",
+                            endorse_checkbox: "enable",
+                        }
+                    ],
+                    "ATN": [
+                        {
+                            endorse_code: "ECN01",
+                            endorse_name: "เปลี่ยนแปลงคำนำหน้าชื่อ ชื่อ - นามสกุล ผู้เอาประกัน",
+                            endorse_checkbox: "enable",
+                        },
+                        {
+                            endorse_code: "ECN02",
+                            endorse_name: "เปลี่ยนแปลงที่อยู่ ผู้เอาประกัน",
+                            endorse_checkbox: "enable",
+                        }
+                    ]
+                },
+            },
+        },
+    },
+    "ORN": {
+        "I": {
+            "ALL": {
+                "0": {
+                    "AGT": [
+                        {
+                            endorse_code: "ECN01",
+                            endorse_name: "เปลี่ยนแปลงคำนำหน้าชื่อ ชื่อ - นามสกุล ผู้เอาประกัน",
+                            endorse_checkbox: "enable",
+                        },
+                        {
+                            endorse_code: "ECN02",
+                            endorse_name: "เปลี่ยนแปลงที่อยู่ ผู้เอาประกัน",
+                            endorse_checkbox: "enable",
+                        }
+                    ]
+                },
+            },
+        },
+    },
+};
