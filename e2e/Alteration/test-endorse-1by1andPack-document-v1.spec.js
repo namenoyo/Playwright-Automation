d97e79a8b8fe8cc452766c918608c34bbdf09151
namenoyo <rangsiman.ph@ocean.co.th>
2025-11-05 11:29:28 +0700

    push
    Update Test Data Pack

--- e2e/Alteration/test-endorse-1by1andPack-document-v1.spec.js
+++ e2e/Alteration/test-endorse-1by1andPack-document-v1.spec.js
@@ -44,7 +44,7 @@ test.describe.configure({ mode: 'parallel' }); // ให้เคสในไฟ
 // จำนวนข้อมูลที่ได้ = endIdx - startIdx
 
 const startIdx = 0;
-const endIdx = 1;
+const endIdx = 3100;
 const testData = data_matrix_save_endorse.slice(startIdx, endIdx); // ตัดข้อมูลตามช่วงที่กำหนด
 
 for (const data_save_endorse of testData) {
