5f429e49977b7351e031fc7a6993da949edf2330
namenoyo <rangsiman.ph@ocean.co.th>
2025-12-01 09:19:08 +0700

    push
    - update

--- e2e/Alteration/test-endorse-1by1andPack-request-document-v1.spec.js
+++ e2e/Alteration/test-endorse-1by1andPack-request-document-v1.spec.js
@@ -48,7 +48,7 @@ test.describe.configure({ mode: 'parallel' }); // ให้เคสในไฟ
 // จำนวนข้อมูลที่ได้ = endIdx - startIdx
 
 const startIdx = 0;
-const endIdx = 1116; // ทั้งหมด 2232 เคส  / ท็อปรัน 1117-10000 
+const endIdx = 341; // ทั้งหมด 682 เคส  / ท็อปรัน 342-10000 
 const testData = data_matrix_save_endorse.slice(startIdx, endIdx); // ตัดข้อมูลตามช่วงที่กำหนด
 
 for (const data_save_endorse of testData) {
