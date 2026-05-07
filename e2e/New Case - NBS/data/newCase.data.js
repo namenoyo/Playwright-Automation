ee54e5929f3ba334dbfaedd381303d5da586a9c6
thanakrit.ph <thanakrit.ph@ocean.co.th>
2026-05-07 09:33:38 +0700

    Update Code
    New Case NBS

--- e2e/New Case - NBS/data/newCase.data.js
+++ e2e/New Case - NBS/data/newCase.data.js
@@ -1,69 +1,26 @@
 export const caseDatas = [
   {
-    Team: "New Product3107",
+    Team: "PoyUAT",
     system: "UAT",
-    username: "3107",
-    fullBranch: "3107:บ้านผือ",
-    agentCode: "6100296",
-    agentName: ": นายแสนไอโอ ไอหกสิบ",
+    username: "0500",
+    fullBranch: "0500 : พระนครศรีอยุธยา",
+    agentCode: "3100330",
+    agentName: ": นายเควี ซีโอเอสวี",
     prefix: "นาย",
     gender: "ชาย",
-    name: "จักรภัทร์",
-    surname: "เพชรประเสริฐ",
-    cardType: "บัตรประชาชน",
-    cardNumber: "1754111126288",
-    requestId: "1002000983",
-    temporaryReceipt: "01600069",
-    age: "25",
-    occupation: "6000:ครู, อาจารย์ (เอกชน)",
-    plan: "A10 โอเชี่ยนไลฟ์ เซฟ แอนด์ โพรเทค 88/8",
-    NumOfRider: "1",
-    sMode: "รายปี",
+    name: "ชาย",
+    surname: "กำยำ",
+    cardNumber: "1611647175707",
+    requestId: "1002002032",
+    temporaryReceipt: "01537509",
+    age: "30",
+    occupation: "6100:พนักงานบริษัท",
+    plan: "496 ไทยสมุทรสบายสบาย 30/15",
+    NumOfRider: "0",
+    sMode: "รายเดือน",
     capital: "1,000,000.00",
     NumOfbenef: "1",
-    rider: [
-      {
-        ridername: "CPA Extra",
-        ridercapital: "900,000"
-      }
-    ],
-    benef: [
-      {
-        benefTitle: "นาย",
-        benefName: "พ่อเอ",
-        benefSurname: "ทดสอบ",
-        benefRelation: "บิดา",
-        benefAge: "66"
-      }
-    ]
-  },
-  {
-    Team: "New Product3107",
-    system: "UAT",
-    username: "3107",
-    fullBranch: "3107:บ้านผือ",
-    agentCode: "6100296",
-    agentName: ": นายแสนไอโอ ไอหกสิบ",
-    prefix: "น.ส.",
-    gender: "หญิง",
-    name: "ปฐมา",
-    surname: "วงศ์วโรทัย",
-    cardNumber: "1754111126385",
-    requestId: "1002000984",
-    temporaryReceipt: "01600070",
-    age: "30",
-    occupation: "2000:ขับรถพยาบาล",
-    plan: "218 สบายชัวร์",
-    NumOfRider: "1",
-    sMode: "รายปี",
-    capital: "800,000.00",
-    NumOfbenef: "1",
-    rider: [
-      {
-        ridername: "CPA2.6",
-        ridercapital: "600,000"
-      }
-    ],
+    rider: [],
     benef: [
       {
         benefTitle: "นาย",
